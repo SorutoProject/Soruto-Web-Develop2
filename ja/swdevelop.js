e150942102aedd1d100fb7f7140b3aaf20e40b25
SorutoProject <32834893+SorutoProject@users.noreply.github.com>
2018-04-03 16:05:48 +0900

    Add files via upload

--- ja/swdevelop.js
+++ ja/swdevelop.js
@@ -43,7 +43,9 @@ iframe.contentDocument.designMode = 'on';
 document.getElementById("submenu").style.display = "none";
 document.getElementById("loader").style.display = "none";
 var config = localStorage.swdConfig.split(',');
-myCodeMirror.setOption("lineWrapping",config[0]);
+	if(config[0] == "true"){autowrapdata = true;}
+	else{autowrapdata=false;}
+myCodeMirror.setOption("lineWrapping",autowrapdata);
 var editorMain = document.getElementsByClassName('CodeMirror')[0];
 editorMain.style.fontFamily = config[1];
 editorMain.style.fontSize = config[2];
@@ -252,10 +254,12 @@ function changeLang(l,v){
 		cMenu();
 }
 function showConfig(){
-	var screen = '<div style="position:absolute;top:0px;left:0px;background:#fefefe;"><b>設定</b>&nbsp;<input type="button" value="保存" onclick="saveConfig();">&nbsp;<input type="button" value="キャンセル" onclick="so.modal.close();"></div><br><span style="font-size:8pt;">※ *のついている設定は次回アクセス時に有効になります</span><br><b>自動改行の設定</b><br><label><input type="checkbox" id="configWrap">自動改行を有効にする</label><br><b>フォントの設定*</b><br>フォント名<br><input type="text" id="configFontFamily" style="width:270px;" placeholder="フォント名(例:メイリオ)"><br>フォントサイズ<br><input type="number" id="configFontSize" style="width:100px;">pt';
+	var screen = '<div style="position:absolute;top:0px;left:0px;background:#fefefe;"><b>設定</b>&nbsp;<input type="button" value="保存" onclick="saveConfig();">&nbsp;<input type="button" value="キャンセル" onclick="so.modal.close();"></div><br><span style="font-size:8pt;">※ <b>*</b>のついている設定は次回アクセス時に有効になります</span><br><b>自動改行の設定</b><br><label><input type="checkbox" id="configWrap">自動改行を有効にする</label><br><b>フォントの設定*</b><br>フォント名<br><input type="text" id="configFontFamily" style="width:270px;" placeholder="フォント名(例:メイリオ)"><br>フォントサイズ<br><input type="number" id="configFontSize" style="width:100px;">pt';
 	so.modal.custom(screen);
 	var nowconfig = localStorage.swdConfig.split(',');
-	document.getElementById("configWrap").checked = nowconfig[0];
+	if(nowconfig[0] == "true"){autowrapdata = true;}
+	else{autowrapdata=false;}
+	document.getElementById("configWrap").checked = autowrapdata;
 	document.getElementById('configFontFamily').value = nowconfig[1];
 	document.getElementById('configFontSize').value = nowconfig[2];
 	cMenu();
