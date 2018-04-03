ad6bcef0689f925020470b005655da36a78a7bd0
SorutoProject <32834893+SorutoProject@users.noreply.github.com>
2018-04-03 20:58:17 +0900

    Add files via upload

--- ja/swdevelop.js
+++ ja/swdevelop.js
@@ -275,7 +275,7 @@ function changeLang(l,v){
 		cMenu();
 }
 function showConfig(){
-	var screen = '<div style="position:absolute;top:0px;left:0px;background:#fefefe;"><b>設定</b>&nbsp;<input type="button" value="保存" onclick="saveConfig();">&nbsp;<input type="button" value="保存せず閉じる" onclick="so.modal.close();"></div><hr color="#c4c4c4"><b>自動改行の設定</b><br><label><input type="checkbox" id="configWrap">自動改行を有効にする</label><hr color="#c4c4c4"><b>フォントの設定</b><br>フォント名<br><input type="text" id="configFontFamily" style="width:270px;" placeholder="フォント名(例:メイリオ)"><br>フォントサイズ<br><input type="number" id="configFontSize" style="width:100px;">pt';
+	var screen = '<b>設定</b>&nbsp;<input type="button" value="保存" onclick="saveConfig();">&nbsp;<input type="button" value="保存せず閉じる" onclick="so.modal.close();"><hr color="#c4c4c4"><b>自動改行の設定</b><br><label><input type="checkbox" id="configWrap">自動改行を有効にする</label><hr color="#c4c4c4"><b>フォントの設定</b><br>フォント名<br><input type="text" id="configFontFamily" style="width:270px;" placeholder="フォント名(例:メイリオ)"><br>フォントサイズ<br><input type="number" id="configFontSize" style="width:100px;">pt';
 	so.modal.custom(screen);
 	var nowconfig = localStorage.swdConfig.split(',');
 	if(nowconfig[0] == "true"){autowrapdata = true;}
