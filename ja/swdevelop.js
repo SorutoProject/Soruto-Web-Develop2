384388069f36ca2414d2e7dc8d20f0700aa17451
SorutoProject <32834893+SorutoProject@users.noreply.github.com>
2018-04-15 22:35:26 +0900

    Add files via upload

--- ja/swdevelop.js
+++ ja/swdevelop.js
@@ -4,7 +4,7 @@
 */
 
 //バージョンを定義
-var version = "3.21";
+var version = "3.211";
 
 var mixedMode = {
         name: "htmlmixed",
@@ -207,7 +207,7 @@ function menu(num){
 		//言語
 		sub.innerHTML='<a href="javascript:void(0);" onclick="changeLang(\'htmlmixed\',\'dv\')" class="submenulink" id="langhtmlmixed">HTML</a><a href="javascript:void(0);" onclick="changeLang(\'javascript\',\'sv\')" class="submenulink" id="langjavascript">JavaScript</a><a href="javascript:void(0);" onclick="changeLang(\'css\',\'sv\')" class="submenulink" id="langcss">CSS</a><a href="javascript:void(0);" onclick="changeLang(\'php\',\'sv\')" class="submenulink" id="langphp">PHP</a><a href="javascript:void(0);" onclick="changeLang(\'xml\',\'sv\')" class="submenulink" id="langxml">XML</a><a href="javascript:void(0);" onclick="changeLang(\'markdown\',\'mdv\')" class="submenulink" id="langmarkdown">MarkDown(MD)</a><a href="javascript:void(0);" class="submenulink" onclick="cMenu();">(メニューを閉じる)</a>';
 		var changelang = myCodeMirror.getOption("mode");
-		document.getElementById("lang" + changelang).style.background = "#138200";
+		document.getElementById("lang" + changelang).style.background = "#2c2c2c";
 	}
 	else if(num==4){
 		//About
